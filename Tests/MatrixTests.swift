0a6599b416afb2454a4786579031be1f36c48e0a
otabuzzman <iuergen.schuck@gmail.com>
2023-08-26 15:26:05 +0200

    Update matrix tests for Acceleration

--- Tests/MatrixTests.swift
+++ Tests/MatrixTests.swift
@@ -12,6 +12,14 @@ final class MatrixTests: TestCase {
         ].enumerated().forEach { (testCaseIndex, testCase) in
             AssertEqual(testCase.result, other: testCase.A, message: "#\(testCaseIndex + 1) failed")
         }
+        [
+            (A: Matrix<Float>(rows: 2, columns: 3, entries: [1, 2, 3, 4, 5, 6]), result: Matrix<Float>(rows: 2, columns: 3, entries: [1, 2, 3, 4, 5, 6])),
+            (A: Matrix<Float>(rows: 2, columns: 3), result: Matrix<Float>(rows: 2, columns: 3, entries: [0, 0, 0, 0, 0, 0])),
+            (A: Matrix<Float>(rows: 3), result: Matrix<Float>(rows: 3, columns: 1, entries: [0, 0, 0])),
+            (A: Matrix<Float>(columns: 3), result: Matrix<Float>(rows: 1, columns: 3, entries: [0, 0, 0]))
+        ].enumerated().forEach { (testCaseIndex, testCase) in
+            AssertEqual(testCase.result, other: testCase.A, message: "#\(testCaseIndex + 1) failed")
+        }
     }
     
     internal func testMatrixTransform() {
@@ -20,6 +28,17 @@ final class MatrixTests: TestCase {
             (A: Matrix<Int>(rows: 2, columns: 2, entries: [-1, 2, -3, 4]), result: Matrix<Int>(rows: 2, columns: 2, entries: [0, 0, 0, 0])),
             (A: Matrix<Int>(rows: 1, columns: 3, entries: [1, -2, 3]), result: Matrix<Int>(rows: 1, columns: 3, entries: [0, 0, 0])),
             (A: Matrix<Int>(rows: 3, columns: 1, entries: [3, -2, 1]), result: Matrix<Int>(rows: 3, columns: 1, entries: [0, 0, 0]))
+        ].enumerated().forEach { (testCaseIndex, testCase) in
+            let A = testCase.A.map { entry in
+                 entry - entry
+            }
+            AssertEqual(testCase.result, other: A, message: "#\(testCaseIndex + 1) failed")
+        }
+        [
+            (A: Matrix<Float>(rows: 2, columns: 3, entries: [1, -2, 3, 4, -5, 6]), result: Matrix<Float>(rows: 2, columns: 3, entries: [0, 0, 0, 0, 0, 0])),
+            (A: Matrix<Float>(rows: 2, columns: 2, entries: [-1, 2, -3, 4]), result: Matrix<Float>(rows: 2, columns: 2, entries: [0, 0, 0, 0])),
+            (A: Matrix<Float>(rows: 1, columns: 3, entries: [1, -2, 3]), result: Matrix<Float>(rows: 1, columns: 3, entries: [0, 0, 0])),
+            (A: Matrix<Float>(rows: 3, columns: 1, entries: [3, -2, 1]), result: Matrix<Float>(rows: 3, columns: 1, entries: [0, 0, 0]))
         ].enumerated().forEach { (testCaseIndex, testCase) in
             let A = testCase.A.map { entry in
                  entry - entry
@@ -38,9 +57,33 @@ final class MatrixTests: TestCase {
             let B = testCase.A.T
             AssertEqual(testCase.result, other: B, message: "#\(testCaseIndex + 1) failed")
         }
+        [
+            (A: Matrix<Float>(rows: 2, columns: 3, entries: [1, 2, 3, 4, 5, 6]), result: Matrix<Float>(rows: 3, columns: 2, entries: [1, 4, 2, 5, 3, 6])),
+            (A: Matrix<Float>(rows: 2, columns: 2, entries: [1, 2, 3, 4]), result: Matrix<Float>(rows: 2, columns: 2, entries: [1, 3, 2, 4])),
+            (A: Matrix<Float>(rows: 1, columns: 3, entries: [1, 2, 3]), result: Matrix<Float>(rows: 3, columns: 1, entries: [1, 2, 3])),
+            (A: Matrix<Float>(rows: 3, columns: 1, entries: [3, 2, 1]), result: Matrix<Float>(rows: 1, columns: 3, entries: [3, 2, 1]))
+        ].enumerated().forEach { (testCaseIndex, testCase) in
+            let B = testCase.A.T
+            AssertEqual(testCase.result, other: B, message: "#\(testCaseIndex + 1) failed")
+        }
     }
     
     internal func testMatrixScalarAddition () {
+        [
+            (s: Int(2), A: Matrix<Int>(rows: 2, columns: 3, entries: [1, 2, 3, 4, 5, 6]), result: Matrix<Int>(rows: 2, columns: 3, entries: [3, 4, 5, 6, 7, 8])),
+            (s: Int(2), A: Matrix<Int>(rows: 2, columns: 2, entries: [1, 2, 3, 4]), result: Matrix<Int>(rows: 2, columns: 2, entries: [3, 4, 5, 6])),
+            (s: Int(2), A: Matrix<Int>(rows: 1, columns: 3, entries: [1, 2, 3]), result: Matrix<Int>(rows: 1, columns: 3, entries: [3, 4, 5])),
+            (s: Int(2), A: Matrix<Int>(rows: 3, columns: 1, entries: [1, 2, 3]), result: Matrix<Int>(rows: 3, columns: 1, entries: [3, 4, 5])),
+            (s: Int(-2), A: Matrix<Int>(rows: 2, columns: 3, entries: [1, 2, 3, 4, 5, 6]), result: Matrix<Int>(rows: 2, columns: 3, entries: [-1, 0, 1, 2, 3, 4])),
+            (s: Int(-2), A: Matrix<Int>(rows: 2, columns: 2, entries: [1, 2, 3, 4]), result: Matrix<Int>(rows: 2, columns: 2, entries: [-1, 0, 1, 2])),
+            (s: Int(-2), A: Matrix<Int>(rows: 1, columns: 3, entries: [1, 2, -3]), result: Matrix<Int>(rows: 1, columns: 3, entries: [-1, 0, -5])),
+            (s: Int(-2), A: Matrix<Int>(rows: 3, columns: 1, entries: [-1, 2, 3]), result: Matrix<Int>(rows: 3, columns: 1, entries: [-3, 0, 1]))
+        ].enumerated().forEach { (testCaseIndex, testCase) in
+            var B = testCase.s + testCase.A
+            AssertEqual(testCase.result, other: B, message: "#\(testCaseIndex + 1) failed")
+            B = testCase.A + testCase.s
+            AssertEqual(testCase.result, other: B, message: "#\(testCaseIndex + 1) failed")
+        }
         [
             (s: Float(2.0), A: Matrix<Float>(rows: 2, columns: 3, entries: [1.0, 2.0, 3.0, 4.0, 5.0, 6.0]), result: Matrix<Float>(rows: 2, columns: 3, entries: [3.0, 4.0, 5.0, 6.0, 7.0, 8.0])),
             (s: Float(2.0), A: Matrix<Float>(rows: 2, columns: 2, entries: [1.0, 2.0, 3.0, 4.0]), result: Matrix<Float>(rows: 2, columns: 2, entries: [3.0, 4.0, 5.0, 6.0])),
@@ -67,9 +110,30 @@ final class MatrixTests: TestCase {
             let C = testCase.A + testCase.B
             AssertEqual(testCase.result, other: C, message: "#\(testCaseIndex + 1) failed")
         }
+        [
+            (A: Matrix<Float>(rows: 2, columns: 3, entries: [1, 2, 3, 4, 5, 6]), B: Matrix<Float>(rows: 2, columns: 3, entries: [1, 4, 2, 5, 3, 6]), result: Matrix<Float>(rows: 2, columns: 3, entries: [2, 6, 5, 9, 8, 12])),
+            (A: Matrix<Float>(rows: 1, columns: 3, entries: [1, 2, 3]), B: Matrix<Float>(rows: 1, columns: 3, entries: [1, 2, 3]), result: Matrix<Float>(rows: 1, columns: 3, entries: [2, 4, 6])),
+            (A: Matrix<Float>(rows: 3, columns: 1, entries: [1, 2, 3]), B: Matrix<Float>(rows: 3, columns: 1, entries: [1, -2, 3]), result: Matrix<Float>(rows: 3, columns: 1, entries: [2, 0, 6]))
+        ].enumerated().forEach { (testCaseIndex, testCase) in
+            let C = testCase.A + testCase.B
+            AssertEqual(testCase.result, other: C, message: "#\(testCaseIndex + 1) failed")
+        }
     }
     
     internal func testScalarMinusMatrix () {
+        [
+            (s: Int(2), A: Matrix<Int>(rows: 2, columns: 3, entries: [1, 2, 3, 4, 5, 6]), result: Matrix<Int>(rows: 2, columns: 3, entries: [1, 0, -1, -2, -3, -4])),
+            (s: Int(2), A: Matrix<Int>(rows: 2, columns: 2, entries: [1, 2, 3, 4]), result: Matrix<Int>(rows: 2, columns: 2, entries: [1, 0, -1, -2])),
+            (s: Int(2), A: Matrix<Int>(rows: 1, columns: 3, entries: [1, 2, 3]), result: Matrix<Int>(rows: 1, columns: 3, entries: [1, 0, -1])),
+            (s: Int(2), A: Matrix<Int>(rows: 3, columns: 1, entries: [1, 2, 3]), result: Matrix<Int>(rows: 3, columns: 1, entries: [1, 0, -1])),
+            (s: Int(-2), A: Matrix<Int>(rows: 2, columns: 3, entries: [1, 2, 3, 4, 5, 6]), result: Matrix<Int>(rows: 2, columns: 3, entries: [-3, -4, -5, -6, -7, -8])),
+            (s: Int(-2), A: Matrix<Int>(rows: 2, columns: 2, entries: [1, 2, 3, 4]), result: Matrix<Int>(rows: 2, columns: 2, entries: [-3, -4, -5, -6])),
+            (s: Int(-2), A: Matrix<Int>(rows: 1, columns: 3, entries: [-1, 2, 3]), result: Matrix<Int>(rows: 1, columns: 3, entries: [-1, -4, -5])),
+            (s: Int(-2), A: Matrix<Int>(rows: 3, columns: 1, entries: [1, 2, -3]), result: Matrix<Int>(rows: 3, columns: 1, entries: [-3, -4, 1]))
+        ].enumerated().forEach { (testCaseIndex, testCase) in
+            let B = testCase.s - testCase.A
+            AssertEqual(testCase.result, other: B, message: "#\(testCaseIndex + 1) failed")
+        }
         [
             (s: Float(2.0), A: Matrix<Float>(rows: 2, columns: 3, entries: [1.0, 2.0, 3.0, 4.0, 5.0, 6.0]), result: Matrix<Float>(rows: 2, columns: 3, entries: [1.0, 0, -1.0, -2.0, -3.0, -4.0])),
             (s: Float(2.0), A: Matrix<Float>(rows: 2, columns: 2, entries: [1.0, 2.0, 3.0, 4.0]), result: Matrix<Float>(rows: 2, columns: 2, entries: [1.0, 0, -1.0, -2.0])),
@@ -86,6 +150,19 @@ final class MatrixTests: TestCase {
     }
     
     internal func testMatrixMinusScalar () {
+        [
+            (s: Int(2), A: Matrix<Int>(rows: 2, columns: 3, entries: [1, 2, 3, 4, 5, 6]), result: Matrix<Int>(rows: 2, columns: 3, entries: [-1, 0, 1, 2, 3, 4])),
+            (s: Int(2), A: Matrix<Int>(rows: 2, columns: 2, entries: [1, 2, 3, 4]), result: Matrix<Int>(rows: 2, columns: 2, entries: [-1, 0, 1, 2])),
+            (s: Int(2), A: Matrix<Int>(rows: 1, columns: 3, entries: [1, 2, 3]), result: Matrix<Int>(rows: 1, columns: 3, entries: [-1, 0, 1])),
+            (s: Int(2), A: Matrix<Int>(rows: 3, columns: 1, entries: [1, 2, 3]), result: Matrix<Int>(rows: 3, columns: 1, entries: [-1, 0, 1])),
+            (s: Int(-2), A: Matrix<Int>(rows: 2, columns: 3, entries: [1, 2, 3, 4, 5, 6]), result: Matrix<Int>(rows: 2, columns: 3, entries: [3, 4, 5, 6, 7, 8])),
+            (s: Int(-2), A: Matrix<Int>(rows: 2, columns: 2, entries: [1, 2, 3, 4]), result: Matrix<Int>(rows: 2, columns: 2, entries: [3, 4, 5, 6])),
+            (s: Int(-2), A: Matrix<Int>(rows: 1, columns: 3, entries: [1, 2, -3]), result: Matrix<Int>(rows: 1, columns: 3, entries: [3, 4, -1])),
+            (s: Int(-2), A: Matrix<Int>(rows: 3, columns: 1, entries: [1, -2, 3]), result: Matrix<Int>(rows: 3, columns: 1, entries: [3, 0, 5]))
+        ].enumerated().forEach { (testCaseIndex, testCase) in
+            let B = testCase.A - testCase.s
+            AssertEqual(testCase.result, other: B, message: "#\(testCaseIndex + 1) failed")
+        }
         [
             (s: Float(2.0), A: Matrix<Float>(rows: 2, columns: 3, entries: [1.0, 2.0, 3.0, 4.0, 5.0, 6.0]), result: Matrix<Float>(rows: 2, columns: 3, entries: [-1.0, 0, 1.0, 2.0, 3.0, 4.0])),
             (s: Float(2.0), A: Matrix<Float>(rows: 2, columns: 2, entries: [1.0, 2.0, 3.0, 4.0]), result: Matrix<Float>(rows: 2, columns: 2, entries: [-1.0, 0, 1.0, 2.0])),
@@ -110,9 +187,32 @@ final class MatrixTests: TestCase {
             let C = testCase.A - testCase.B
             AssertEqual(testCase.result, other: C, message: "#\(testCaseIndex + 1) failed")
         }
+        [
+            (A: Matrix<Float>(rows: 2, columns: 3, entries: [1, 2, 3, 4, 5, 6]), B: Matrix<Float>(rows: 2, columns: 3, entries: [1, 4, 2, 5, 3, 6]), result: Matrix<Float>(rows: 2, columns: 3, entries: [0, -2, 1, -1, 2, 0])),
+            (A: Matrix<Float>(rows: 1, columns: 3, entries: [1, 2, 3]), B: Matrix<Float>(rows: 1, columns: 3, entries: [1, 2, 3]), result: Matrix<Float>(rows: 1, columns: 3, entries: [0, 0, 0])),
+            (A: Matrix<Float>(rows: 3, columns: 1, entries: [1, 2, 3]), B: Matrix<Float>(rows: 3, columns: 1, entries: [1, -2, 3]), result: Matrix<Float>(rows: 3, columns: 1, entries: [0, 4, 0]))
+        ].enumerated().forEach { (testCaseIndex, testCase) in
+            let C = testCase.A - testCase.B
+            AssertEqual(testCase.result, other: C, message: "#\(testCaseIndex + 1) failed")
+        }
     }
     
     internal func testMatrixScalarMultiplication () {
+        [
+            (s: Int(2), A: Matrix<Int>(rows: 2, columns: 3, entries: [1, 2, 3, 4, 5, 6]), result: Matrix<Int>(rows: 2, columns: 3, entries: [2, 4, 6, 8, 10, 12])),
+            (s: Int(2), A: Matrix<Int>(rows: 2, columns: 2, entries: [1, 2, 3, 4]), result: Matrix<Int>(rows: 2, columns: 2, entries: [2, 4, 6, 8])),
+            (s: Int(2), A: Matrix<Int>(rows: 1, columns: 3, entries: [1, 2, 3]), result: Matrix<Int>(rows: 1, columns: 3, entries: [2, 4, 6])),
+            (s: Int(2), A: Matrix<Int>(rows: 3, columns: 1, entries: [1, 2, 3]), result: Matrix<Int>(rows: 3, columns: 1, entries: [2, 4, 6])),
+            (s: Int(-2), A: Matrix<Int>(rows: 2, columns: 3, entries: [1, 2, 3, 4, 5, 6]), result: Matrix<Int>(rows: 2, columns: 3, entries: [-2, -4, -6, -8, -10, -12])),
+            (s: Int(-2), A: Matrix<Int>(rows: 2, columns: 2, entries: [1, 2, 3, 4]), result: Matrix<Int>(rows: 2, columns: 2, entries: [-2, -4, -6, -8])),
+            (s: Int(-2), A: Matrix<Int>(rows: 1, columns: 3, entries: [1, -2, 3]), result: Matrix<Int>(rows: 1, columns: 3, entries: [-2, 4, -6])),
+            (s: Int(-2), A: Matrix<Int>(rows: 3, columns: 1, entries: [1, -2, 3]), result: Matrix<Int>(rows: 3, columns: 1, entries: [-2, 4, -6]))
+        ].enumerated().forEach { (testCaseIndex, testCase) in
+            var B = testCase.s * testCase.A
+            AssertEqual(testCase.result, other: B, message: "#\(testCaseIndex + 1) failed")
+            B = testCase.A * testCase.s
+            AssertEqual(testCase.result, other: B, message: "#\(testCaseIndex + 1) failed")
+        }
         [
             (s: Float(2.0), A: Matrix<Float>(rows: 2, columns: 3, entries: [1.0, 2.0, 3.0, 4.0, 5.0, 6.0]), result: Matrix<Float>(rows: 2, columns: 3, entries: [2.0, 4.0, 6.0, 8.0, 10.0, 12.0])),
             (s: Float(2.0), A: Matrix<Float>(rows: 2, columns: 2, entries: [1.0, 2.0, 3.0, 4.0]), result: Matrix<Float>(rows: 2, columns: 2, entries: [2.0, 4.0, 6.0, 8.0])),
