5d01665dfc69f07e2556b75380b9105370b58e2f
Jürgen Schuck <otabuzzman@users.noreply.github.com>
2023-09-02 20:52:20 +0200

    Rework CustomCoder tests

--- Tests/CustomCoderTests.swift
+++ Tests/CustomCoderTests.swift
@@ -5,12 +5,12 @@ import Foundation
 final class CustomCoderTests: TestCase {
     internal func testStringCoder() {
         [
-            (input: "", result: ""),
-            (input: "ABC", result: "ABC"),
-            (input: "123", result: "123")
+            (input: "", expect: ""),
+            (input: "ABC", expect: "ABC"),
+            (input: "123", expect: "123")
         ].enumerated().forEach { (testCaseIndex, testCase) in
-            let x = String(from: testCase.input.encode)
-            AssertEqual(testCase.result, other: x, message: "#\(testCaseIndex + 1) failed")
+            let result = String(from: testCase.input.encode)
+            AssertEqual(testCase.expect, other: result, message: "#\(testCaseIndex + 1) failed")
         }
     }
     
@@ -19,15 +19,15 @@ final class CustomCoderTests: TestCase {
             4711, -4711, Int.min, Int.max
         ].enumerated().forEach { (testCaseIndex, testCase) in
             let input = testCase.encode
-            let result = Int(from: input) ?? 0
-            AssertEqual(result, other: testCase, message: "#\(testCaseIndex + 1) failed")
+            let expect = Int(from: input) ?? 0
+            AssertEqual(expect, other: testCase, message: "#\(testCaseIndex + 1) failed")
         }
         [
             4711.0815, -4711.0815, Float.infinity
         ].enumerated().forEach { (testCaseIndex, testCase) in
             let input = testCase.encode
-            let result = Float(from: input) ?? 0
-            AssertEqual(result, other: testCase, message: "#\(testCaseIndex + 1) failed")
+            let expect = Float(from: input) ?? 0
+            AssertEqual(expect, other: testCase, message: "#\(testCaseIndex + 1) failed")
         }
     }
 }
