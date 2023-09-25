379a34efee33a86a8bb88484f5edb2c801bd53c8
Jürgen Schuck <otabuzzman@users.noreply.github.com>
2023-09-25 21:14:10 +0200

    Save work on improved UI

--- AIplay.swift
+++ AIplay.swift
@@ -2,11 +2,163 @@ import SwiftUI
 import PlaygroundTester
 
 struct ContentView: View {
+    @State private var showAppInfo = false
+    
     var body: some View {
+        HStack {
+            Label("AIplay", systemImage: "square")
+            Spacer()
+            Button {
+                showAppInfo.toggle()
+            } label: {
+                Image(systemName: "info.circle")
+            }
+        }
+        .font(.title)
+        .sheet(isPresented: $showAppInfo) {
+            appInfo(isPresented: $showAppInfo)
+        }
+        ProgressView(value: 0)
         VStack {
-            AppIcon()
-                .frame(width: 100, height: 100, alignment: .center)
-            MYONNView()
+            Form {
+                Section {
+                    HStack {
+                        Button {
+                        } label: {
+                            Label("Reload MNIST", systemImage: "arrow.counterclockwise")
+                        }
+                        Spacer()
+                        Circle()
+                            .frame(height: 28)
+                            .foregroundStyle(.gray)
+                    }
+                } header: {
+                    HStack {
+                        Label("DATASET", systemImage: "chart.bar").font(.headline)
+                        Spacer()
+                    }
+                }
+                Section {
+                    HStack {
+                        Text("Mini-batch size")
+                        Spacer()
+                        Text("30")
+                    }
+                    HStack {
+                        Text("Learning rate")
+                        Spacer()
+                        Text("0.3")
+                    }
+                } header: {
+                    HStack {
+                        Label("NN CONFIGURATION", systemImage: "gearshape").font(.headline)
+                        Spacer()
+                    }
+                }
+                Section {
+                    HStack {
+                        Text("Epochs trained so far")
+                        Spacer()
+                        Text("0")
+                    }
+                    HStack {
+                        Text("Duration of last completion")
+                        Spacer()
+                        Text("0:00")
+                    }
+                } header: {
+                    HStack {
+                        Label("NN TRAINING", systemImage: "dumbbell").font(.headline)
+                        Spacer()
+                    }
+                }
+                Section {
+                    HStack {
+                        Button {
+                        } label: {
+                            Label("Train next mini-batch", systemImage: "figure.strengthtraining.traditional")
+                        }
+                        Spacer()
+                    }
+                    HStack {
+                        Button {
+                        } label: {
+                            Label("Train another epoch", systemImage: "figure.strengthtraining.traditional")
+                        }
+                        Spacer()
+                    }
+                    HStack {
+                        Button {
+                        } label: {
+                            Label("Determine performance", systemImage: "sparkle.magnifyingglass")
+                        }
+                        Spacer()
+                        Text("0.9442")
+                    }
+                    HStack {
+                        Button {
+                        } label: {
+                            Label("Import model from Files", systemImage: "square.and.arrow.up")
+                        }
+                        Spacer()
+                    }
+                    HStack {
+                        Button {
+                        } label: {
+                            Label("Export model to Files", systemImage: "square.and.arrow.down")
+                        }
+                        Spacer()
+                    }
+                }
+                Section {
+                    HStack {
+                        Button {
+                        } label: {
+                            Label("Random item", systemImage: "sparkle.magnifyingglass")
+                        }
+                        Spacer()
+                        Text("4")
+                    }
+                    HStack {
+                        Image(systemName: "square.and.pencil")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                        Image(systemName: "sparkle.magnifyingglass")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                    }
+                } header: {
+                    HStack {
+                        Label("NN PREDICTION", systemImage: "wand.and.stars.inverse").font(.headline)
+                        Spacer()
+                    }
+                }
+            }
+        }
+    }
+}
+
+func appInfo(isPresented: Binding<Bool>) -> some View {
+    let description = try! AttributedString(markdown:
+            """
+            """, options: .init(interpretedSyntax: .inlineOnlyPreservingWhitespace))
+    return infoView(isPresented: isPresented, Text(description))
+}
+
+func infoView(isPresented: Binding<Bool>, _ description: Text) -> some View {
+    VStack {
+        HStack {
+            Text("DESCRIPTION")
+                .font(.headline)
+                .padding()
+            Spacer()
+        }
+        description
+            .lineLimit(nil)
+        Button {
+            isPresented.wrappedValue.toggle()
+        } label: {
+            Text("Close")
         }
     }
 }
